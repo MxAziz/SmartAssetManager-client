f22f8260621725ae0953ef07e4d4e16728f4322f
MxAziz <hereabdulaziz@gmail.com>
2025-02-20 12:56:47 +0600

    dark mode implement on HR Manager registration page

--- src/Pages/Login/JoinHr.jsx
+++ src/Pages/Login/JoinHr.jsx
@@ -76,131 +76,143 @@ const JoinHr = () => {
   };
 
   return (
-    <div className="mt-[70px] mb-16 w-3/4 mx-auto flex ">
-      <Helmet>
-        <title>JoinHr - SmartAssetManager</title>
-      </Helmet>
-      {/* image */}
-      <figure className="hidden lg:block">
-        <img
-          className=" h-full w-[490px] shadow-2xl rounded-l-lg rounded-br-lg"
-          src="https://i.pinimg.com/736x/4d/32/95/4d32957d2fc2d92c6893e2cfef3b7bf8.jpg"
-        />
-      </figure>
-      {/* login form */}
-      <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
-        <form onSubmit={handleSubmit} className="card-body ">
-          <div className="flex flex-col items-center">
-            <h3 className="text-2xl mt-2">Registration Now !</h3>
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Your Name</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your Name"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Your PhotoURL</span>
-            </label>
-            <input
-              type="text"
-              name="photo"
-              placeholder="Your PhotoURL"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Company Name</span>
-            </label>
-            <input
-              type="text"
-              name="companyName"
-              placeholder="Company Name"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Company Logo</span>
-            </label>
-            <input
-              type="text"
-              name="companyLogo"
-              placeholder="Company Logo"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div>
-            <label className="label-text ml-1">Date of Birth</label>
-            <input
-              type="date"
-              name="dob"
-              required
-              className="w-full px-4 py-2 border rounded-md "
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Subscription Packages </span>
-            </label>
-            <select
-              id="packages"
-              name="packageId"
-              value={selectedValue}
-              onChange={(e) => setSelectedValue(e.target.value)}
-              className="select select-bordered w-full max-w-sm"
-            >
-              <option disabled value="">
-                Select a package?
-              </option>
-              {packages.map((pkg) => (
-                <option key={pkg.id} value={pkg.price}>
-                  {pkg.name} - ${pkg.price}
+    <div className="dark:bg-gray-600 dark:text-white">
+      <div className="pt-[70px] pb-16 w-3/4 mx-auto flex ">
+        <Helmet>
+          <title>JoinHr - SmartAssetManager</title>
+        </Helmet>
+        {/* image */}
+        <figure className="hidden lg:block">
+          <img
+            className=" h-full w-[490px] shadow-2xl rounded-l-lg rounded-br-lg"
+            src="https://i.pinimg.com/736x/4d/32/95/4d32957d2fc2d92c6893e2cfef3b7bf8.jpg"
+          />
+        </figure>
+        {/* login form */}
+        <div className="card bg-base-100 dark:bg-gray-800 dark:text-white w-full max-w-md shrink-0 shadow-2xl">
+          <form onSubmit={handleSubmit} className="card-body ">
+            <div className="flex flex-col items-center">
+              <h3 className="text-2xl mt-2">Registration Now !</h3>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">Your Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">
+                  Your PhotoURL
+                </span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Your PhotoURL"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">
+                  Company Name
+                </span>
+              </label>
+              <input
+                type="text"
+                name="companyName"
+                placeholder="Company Name"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">
+                  Company Logo
+                </span>
+              </label>
+              <input
+                type="text"
+                name="companyLogo"
+                placeholder="Company Logo"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div>
+              <label className="label-text ml-1 dark:text-gray-100">
+                Date of Birth
+              </label>
+              <input
+                type="date"
+                name="dob"
+                required
+                className="w-full px-4 py-2 border rounded-md dark:bg-gray-600 dark:border-none"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">
+                  Subscription Packages{" "}
+                </span>
+              </label>
+              <select
+                id="packages"
+                name="packageId"
+                value={selectedValue}
+                onChange={(e) => setSelectedValue(e.target.value)}
+                className="select select-bordered w-full max-w-sm dark:bg-gray-600"
+              >
+                <option disabled value="">
+                  Select a package?
                 </option>
-              ))}
-            </select>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn bg-[#6e3261] text-white hover:bg-gray-700">
-              SignUp
-            </button>
-          </div>
-        </form>
+                {packages.map((pkg) => (
+                  <option key={pkg.id} value={pkg.price}>
+                    {pkg.name} - ${pkg.price}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#6e3261] border-none text-white hover:bg-gray-700">
+                SignUp
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
