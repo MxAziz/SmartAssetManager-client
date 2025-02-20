4a05fbee1b883df1cb1728ab2c185e92608e5317
MxAziz <hereabdulaziz@gmail.com>
2025-02-20 12:50:23 +0600

    implement dark mode on login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -59,64 +59,66 @@ const Login = () => {
   };
 
   return (
-    <div className="mt-[70px] mb-16 w-2/3 mx-auto flex">
-      <Helmet>
-        <title>Login - SmartAssetManager</title>
-      </Helmet>
-      {/* image */}
-      <figure className='hidden lg:block'>
-        <img
-          className=" h-[510px] w-[450px] shadow-2xl"
-          src="https://i.pinimg.com/736x/54/b9/ef/54b9ef6848259e884dee8a3744706439.jpg"
-          alt=""
-        />
-      </figure>
-      {/* login form */}
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form onSubmit={handleLogin} className="card-body ">
-          <div className="flex flex-col items-center">
-            <h3 className="text-xl mt-2">Welcome Back !</h3>
-            <button
-              onClick={handleGoogleSignIn}
-              className="btn mt-3 w-full flex items-center justify-center gap-2 bg-white text-gray-800 border border-gray-400 rounded-lg px-4 py-2 "
-            >
-              <FcGoogle className="size-5" />
-              Login in with Google
-            </button>
-          </div>
-          <div className="divider text-xs hover:underline">
-            Or Login with Email
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              className="input input-bordered "
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn bg-[#6e3261] text-white hover:bg-gray-700">
-              Login
-            </button>
-          </div>
-        </form>
+    <div className="dark:bg-gray-600">
+      <div className="pt-[110px] pb-16 w-2/3 mx-auto flex">
+        <Helmet>
+          <title>Login - SmartAssetManager</title>
+        </Helmet>
+        {/* image */}
+        <figure className="hidden lg:block">
+          <img
+            className=" h-[510px] w-[450px] shadow-2xl"
+            src="https://i.pinimg.com/736x/54/b9/ef/54b9ef6848259e884dee8a3744706439.jpg"
+            alt=""
+          />
+        </figure>
+        {/* login form */}
+        <div className="card bg-base-100 dark:bg-gray-800 dark:text-white w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body ">
+            <div className="flex flex-col items-center">
+              <h3 className="text-xl mt-2">Welcome Back !</h3>
+              <button
+                onClick={handleGoogleSignIn}
+                className="btn mt-3 w-full flex items-center justify-center gap-2 bg-white dark:bg-gray-600 dark:border-none dark:text-white text-gray-800 border border-gray-400 rounded-lg px-4 py-2 "
+              >
+                <FcGoogle className="size-5" />
+                Login in with Google
+              </button>
+            </div>
+            <div className="divider text-xs hover:underline">
+              Or Login with Email
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text dark:text-gray-100">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered dark:bg-gray-600"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#6e3261] text-white dark:border-none hover:bg-gray-700">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
